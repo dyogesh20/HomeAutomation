b182f652a605920f93a7abb9d5927e23606356d2
dyogesh20 <yogi.dhada@gmail.com>
2016-12-04 23:46:27 -0800

    basic functionality Working, Add FCM

---
+++ app/src/main/java/com/example/yogesh/homeautomation/VerifyAction.java
@@ -0,0 +1,22 @@
+package com.example.yogesh.homeautomation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class VerifyAction {
+    public boolean hasFourChar(String rUserName) {
+        return rUserName.length() > 3;
+    }
+
+    public boolean hasEightChar(String rPassword) {
+        return rPassword.length() > 7;
+    }
+
+    public boolean validEmail(String rEmail) {
+        String emailRegRule = "^(.+)@(.+)$";
+        Pattern emailPattern = Pattern.compile(emailRegRule);
+        Matcher emailMatcher = emailPattern.matcher(rEmail);
+        return ((!rEmail.isEmpty()) && (emailMatcher.matches()));
+    }
+}
